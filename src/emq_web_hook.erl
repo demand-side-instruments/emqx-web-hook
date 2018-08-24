f85add6d07bd32a8ca259e5e0a8c7dbf6b58419d
jfpineau <jf.pineau@dsinstruments.fr>
2018-08-24 21:05:34 +0400

    add logs

--- src/emq_web_hook.erl
+++ src/emq_web_hook.erl
@@ -286,11 +286,14 @@ format_from(_) ->
 format_payload(Payload) when is_binary(Payload) ->
   case catch mochijson2:encode(Payload) of
     % return hash if payload can't be converted into json (binary for exemple)
-    {'EXIT', _} -> null;
+    {'EXIT', _} ->
+      null,
+      logger:info("could not encode to json", Payload);
     Result -> Result
   end.
 format_hash(Payload) when is_binary(Payload) ->
-  <<X:256/big-unsigned-integer>> = crypto:hash(sha256,Payload),
+  logger:info("in hash function", Payload),
+  <<X:256/big-unsigned-integer>> = crypto:hash(sha256,[Payload]),
   % The trick to this expression is the io_lib:format format string for integers. Each format term is introduced through a tilde. There are three (mostly optional) fields: width, precision and pad, followed by the conversion character. "B" means "uppercase integer of given base," and "b" means "lowercase integer of given base." The width in this case is 64 characters, the precision is 16, which is interpreted as base for this conversion, and the padding character is 0. If it didn't pad by 0, then single-digit values would be default padded to 2 width by a space, which is not what we want.
   lists:flatten(io_lib:format("~64.16.0b", [X])).
 a2b(A) -> erlang:atom_to_binary(A, utf8).
